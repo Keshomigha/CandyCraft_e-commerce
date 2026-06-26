9e4f0c2a545c7f50fb4aeb6b938622f25f9396f2
Keshomigha <keshomighasajeeve@gmail.com>
2026-06-26 22:32:27 +0530

    Updated roleMiddleware.js

---
+++ backend/middleware/roleMiddleware.js
@@ -0,0 +1,10 @@
+function restrictTo(...roles) {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Forbidden: insufficient permissions' });
+    }
+    next();
+  };
+}
+
+module.exports = { restrictTo };
