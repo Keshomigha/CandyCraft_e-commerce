3204a6b83b3471b599ba9cbc19d179ef5faaeb9f
Keshomigha <keshomighasajeeve@gmail.com>
2026-09-02 09:22:56 +0530

    Updated productApi.js

--- frontend/src/api/productApi.js
+++ frontend/src/api/productApi.js
@@ -3,3 +3,4 @@ import api from './axiosInstance';
 export const getProducts = (params = {}) => api.get('/api/products', { params });
 export const getProductById = (id) => api.get(`/api/products/${id}`);
 export const getCategories = () => api.get('/api/categories');
+export const getSearchSuggestions = (q, signal) => api.get('/api/products/suggestions', { params: { q }, signal });
